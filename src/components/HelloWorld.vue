43d5d4cd13f317e5bd9a27d9d1ec511d0a796760
alabo-excel <iamalaboexcel@gmail.com>
2020-12-20 02:42:59 +0100

    weather-app-commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,90 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row class="text-center" align="center" justify="center">
+      <v-app-bar app color="primary" dark>
+        <h1>Weather App</h1>
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="country"
+          class="pt-3"
+          @blur="getWeather"
+          label="Enter Country"
+        ></v-text-field>
+      </v-app-bar>
+      <v-col v-if="this.weather === null">
+
+      </v-col>
+      <v-col v-else cols="6">
+        <v-card class="p-5">
+          <div align="center" class="mt-5">
+            <img
+              :src="
+                'http://openweathermap.org/img/wn/' +
+                this.weather.weather[0].icon +
+                '.png'
+              "
+              alt=""
+            />
+            <div>
+              <h2>{{ this.weather.name }}</h2>
+            </div>
+            <div>
+              <p>Lat {{ this.weather.coord.lat }}</p>
+            </div>
+            <div>
+              <p>Lon {{ this.weather.coord.lon }}</p>
+            </div>
+
+            <div>
+              <p>{{ this.weather.weather[0].description }}</p>
+            </div>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import axios from "axios";
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+export default {
+  name: "HelloWorld",
+
+  data: () => ({
+    country: "Nigeria",
+    weather: null,
+    dailyForcast:null
+  }),
+
+  mounted() {
+    axios({
+      url:
+        "http://api.openweathermap.org/data/2.5/weather?q=Nigeria&appid=168b22b6497b371b59f2cfb88691ed86",
+      method: "get",
+    }).then((response) => {
+      // console.log(response.data);
+      this.weather = response.data;
+    });
+  },
+
+  methods: {
+    getWeather() {
+      axios({
+        url:
+          "http://api.openweathermap.org/data/2.5/weather?q=" +
+          this.country +
+          "&appid=168b22b6497b371b59f2cfb88691ed86",
+        method: "get",
+      }).then((response) => {
+        this.weather = response.data;
+      });
+    },
+  },
+};
+</script>
+<style>
+input {
+  width: 50px !important;
 }
 </style>
